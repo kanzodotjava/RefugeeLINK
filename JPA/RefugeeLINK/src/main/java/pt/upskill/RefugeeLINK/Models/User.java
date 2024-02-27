43922d1648132b590776c2032e27244be87b8318
Pedro Mota <motap023@gmail.com>
2024-02-27 16:18:14 +0000

    Started creating User entity

---
+++ JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Models/User.java
@@ -0,0 +1,23 @@
+package pt.upskill.RefugeeLINK.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private boolean refugee;
+
+
+
+}
