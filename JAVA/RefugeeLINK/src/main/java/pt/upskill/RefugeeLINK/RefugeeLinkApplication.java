73fa45f91f88269787dc61597afa201c67bf5af3
Pedro Mota <motap023@gmail.com>
2024-02-27 11:32:44 +0000

    Added JPA start project

---
+++ JAVA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/RefugeeLinkApplication.java
@@ -0,0 +1,13 @@
+package pt.upskill.RefugeeLINK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefugeeLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefugeeLinkApplication.class, args);
+	}
+
+}
---
+++ JAVA/RefugeeLINK/src/test/java/pt/upskill/RefugeeLINK/RefugeeLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.upskill.RefugeeLINK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefugeeLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
