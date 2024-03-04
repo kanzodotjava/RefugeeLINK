ca96177faf0638fb47c73b77679be65e7856db92
franciscomaia097 <105322731+franciscomaia097@users.noreply.github.com>
2024-03-04 16:54:46 +0000

    Angular updates

--- refugee-link-angular/src/app/app.module.ts
+++ refugee-link-angular/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ServicesDisplayComponent } from './services-display/services-display.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent, NavBarComponent, ServicesDisplayComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
