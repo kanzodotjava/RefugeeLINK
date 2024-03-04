77ee50e27d41332d7f1446cf3e48bb10b04c56bc
Pedro Mota <motap023@gmail.com>
2024-03-04 10:15:54 +0000

    Added Enums and Models

--- JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Models/Formation.java
+++ JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Models/Formation.java
@@ -4,20 +4,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 @Entity
-public class User {
+public class Formation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
-    private String password;
-    private boolean refugee;
+    String name;
+    String description;
+    int numberOfLessons;
+    Date startDate;
+    Date endDate;
 
-
-
+    List<Refugee> students;
+    Map<Refugee,Boolean> isApproved;
 }
---
+++ JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Models/Mentor.java
@@ -0,0 +1,11 @@
+package pt.upskill.RefugeeLINK.Models;
+
+import pt.upskill.RefugeeLINK.Enums.Profession;
+import pt.upskill.RefugeeLINK.Enums.Status;
+
+public class Mentor extends Person {
+    private Profession profession;
+    private Refugee refugee;
+    private Status status;
+
+}
