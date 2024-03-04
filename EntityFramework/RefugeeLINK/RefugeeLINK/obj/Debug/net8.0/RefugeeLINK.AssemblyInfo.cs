28fc35b631bdbf1018bd29a6566a20b821f45437
Pedro Mota <motap023@gmail.com>
2024-03-04 13:58:00 +0000

    Deleted sumn

--- EntityFramework/RefugeeLINK/RefugeeLINK/obj/Debug/net8.0/RefugeeLINK.AssemblyInfo.cs
+++ EntityFramework/RefugeeLINK/RefugeeLINK/obj/Debug/net8.0/RefugeeLINK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RefugeeLINK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000f3e54318ef45985ae8063bfa6fd90e807dad3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea4eba4b59233b0726a7401bd57cde4ca4e0644")]
 [assembly: System.Reflection.AssemblyProductAttribute("RefugeeLINK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RefugeeLINK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
