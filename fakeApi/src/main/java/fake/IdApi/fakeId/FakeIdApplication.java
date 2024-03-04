4c0c5e3a7cf56e9a3d5a1b564e8c4a7ca4ccd1c2
Rafa <rafa.figueiredo48@gmail.com>
2024-03-04 15:32:18 +0000

    addded a Fake api to project
    fake api to check for ids

---
+++ fakeApi/src/main/java/fake/IdApi/fakeId/FakeIdApplication.java
@@ -0,0 +1,13 @@
+package fake.IdApi.fakeId;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeIdApplication.class, args);
+	}
+
+}
