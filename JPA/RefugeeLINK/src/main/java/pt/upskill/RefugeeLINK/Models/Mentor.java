5f175a4dbce4e91321f692a4738851050bc22261
Pedro Mota <motap023@gmail.com>
2024-03-04 16:48:28 +0000

    CRAZY CHANGES!

--- JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Models/Mentor.java
+++ JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Models/Mentor.java
@@ -1,13 +1,41 @@
 package pt.upskill.RefugeeLINK.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import pt.upskill.RefugeeLINK.Enums.Profession;
 import pt.upskill.RefugeeLINK.Enums.Status;
+
+import java.util.List;
 
 @Entity
 public class Mentor extends Person{
     Profession profession;
-    Refugee refugee;
+
+    @OneToMany(mappedBy = "mentor")
+    List<Refugee> refugee;
     Status status;
 
+    public Profession getProfession() {
+        return profession;
+    }
+
+    public List<Refugee> getRefugee() {
+        return refugee;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setProfession(Profession profession) {
+        this.profession = profession;
+    }
+
+    public void setRefugee(List<Refugee> refugee) {
+        this.refugee = refugee;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
--- JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Repositories/RefugeeRepository.java
+++ JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Repositories/RefugeeRepository.java
@@ -1,7 +1,9 @@
 package pt.upskill.RefugeeLINK.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pt.upskill.RefugeeLINK.Models.Refugee;
 
+@Repository
 public interface RefugeeRepository extends JpaRepository<Refugee, Long> {
 }
