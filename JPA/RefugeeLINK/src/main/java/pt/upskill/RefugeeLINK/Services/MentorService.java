d1500fc496bf38e5ba90afd519091227f017c19a
CostaPedro96 <costapm96@gmail.com>
2024-03-05 09:45:34 +0000

    Mentor service

--- JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Services/MentorService.java
+++ JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Services/MentorService.java
@@ -1,11 +1,15 @@
 package pt.upskill.RefugeeLINK.Services;
 
 import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import pt.upskill.RefugeeLINK.Models.Mentor;
 import pt.upskill.RefugeeLINK.Repositories.MentorRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -22,8 +26,35 @@ public class MentorService {
     }
 
     public Mentor getMentorById(Long id){
-        return null;
+        if(mentorRepository.existsById(id)){
+            return mentorRepository.findById(id).orElseThrow();
+        }
+        throw new EntityNotFoundException("Mentor with id: " + id + " was not found!");
     }
+
+    public List<Mentor> getAllMentors(){
+        List<Mentor> mentors = mentorRepository.findAll();
+        if(mentors.isEmpty()){
+            System.out.println("No mentors found!");
+        }
+        return mentors;
+    }
+
+    public boolean deleteMentorById(Long id){
+        if(mentorRepository.existsById(id)){
+            mentorRepository.deleteById(id);
+        }
+        return false;
+    }
+
+    public Mentor updateMentor(Long id,Mentor mentor){
+        if(mentorRepository.existsById(mentor.getId())){
+            mentor.setId(id);
+            return mentorRepository.save(mentor);
+        }
+        throw new EntityNotFoundException();
+    }
+
 
 
 }
