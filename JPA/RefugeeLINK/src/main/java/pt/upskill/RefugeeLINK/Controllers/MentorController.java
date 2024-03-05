b25e6930caed25eaabdce5f4b2c5c2ebe5b729a6
CostaPedro96 <costapm96@gmail.com>
2024-03-05 10:18:34 +0000

    Mentor DTO

--- JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Controllers/MentorController.java
+++ JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Controllers/MentorController.java
@@ -1,14 +1,39 @@
 package pt.upskill.RefugeeLINK.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pt.upskill.RefugeeLINK.DTO.MentorDTO;
+import pt.upskill.RefugeeLINK.Models.Mentor;
 import pt.upskill.RefugeeLINK.Services.MentorService;
 
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 @RestController
+@RequestMapping("/mentor")
 public class MentorController {
 
     @Autowired
     MentorService mentorService;
 
+
+    @GetMapping
+    public ResponseEntity<List<MentorDTO>> getAllMentors(){
+        List<Mentor> mentors = mentorService.getAllMentors();
+        List<MentorDTO> mentorDTOs = mentors.stream()
+                .map(Mentor::toDTO)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(mentorDTOs);
+    }
+
+    @PostMapping
+    public ResponseEntity<MentorDTO> createMentor(@RequestBody Mentor mentor){
+        Mentor createdMentor = mentorService.addMentor(mentor);
+        MentorDTO mentorDTO = createdMentor.toDTO();
+        return new ResponseEntity<>(mentorDTO, HttpStatus.CREATED);
+    }
 }
--- JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Models/Mentor.java
+++ JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Models/Mentor.java
@@ -2,6 +2,7 @@ package pt.upskill.RefugeeLINK.Models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import pt.upskill.RefugeeLINK.DTO.MentorDTO;
 import pt.upskill.RefugeeLINK.Enums.Profession;
 import pt.upskill.RefugeeLINK.Enums.Status;
 
@@ -11,12 +12,17 @@ import java.util.List;
 public class Mentor extends Person{
     Profession profession;
 
+    String description;
     @OneToMany(mappedBy = "mentor")
     List<Refugee> refugee;
     Status status;
 
     public Profession getProfession() {
         return profession;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public List<Refugee> getRefugee() {
@@ -38,4 +44,28 @@ public class Mentor extends Person{
     public void setStatus(Status status) {
         this.status = status;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public MentorDTO toDTO() {
+        MentorDTO mentorDTO = new MentorDTO();
+        mentorDTO.setId(this.getId());
+        mentorDTO.setName(this.getName());
+        mentorDTO.setEmailAddress(this.getEmailAddress());
+        mentorDTO.setBirthdayDate(this.getBirthdayDate());
+        mentorDTO.setPhoneNumber(this.getPhoneNumber());
+        mentorDTO.setCountry(this.getCountry());
+        mentorDTO.setUserName(this.getUserName());
+        mentorDTO.setPassword(this.getPassword());
+        mentorDTO.setNationality(this.getNationality());
+        mentorDTO.setPrimaryLanguage(this.getPrimaryLanguage());
+        mentorDTO.setSecondaryLanguage(this.getSecondaryLanguage());
+        mentorDTO.setCitizenCard(this.getCitizenCard());
+        mentorDTO.setProfession(this.profession);
+        mentorDTO.setStatus(this.status);
+        return mentorDTO;
+    }
+
 }
