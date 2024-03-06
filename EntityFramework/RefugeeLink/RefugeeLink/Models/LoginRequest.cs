904158098135147e179a72cdf066adaa01110e33
CostaPedro96 <costapm96@gmail.com>
2024-03-06 16:26:07 +0000

    Entity frame

--- EntityFramework/RefugeeLink/RefugeeLink/Models/LoginRequest.cs
+++ EntityFramework/RefugeeLink/RefugeeLink/Models/LoginRequest.cs
@@ -1,9 +1,12 @@
-﻿namespace RefugeeLink.Models
+﻿using System.Text.Json.Serialization;
+
+namespace RefugeeLink.Models
 {
     public class LoginRequest
     {
-
+        [JsonPropertyName("userName")]
         public string? UserName { get; set; }
+        [JsonPropertyName("password")]
         public string? Password { get; set; }
 
     }
