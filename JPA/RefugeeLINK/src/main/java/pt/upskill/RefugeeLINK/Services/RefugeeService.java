bbc7c7eeb1edde22d9234ea2937c9e7b00f8b7eb
CostaPedro96 <costapm96@gmail.com>
2024-03-08 13:22:44 +0000

    mentor exception

--- JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Services/RefugeeService.java
+++ JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Services/RefugeeService.java
@@ -5,6 +5,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import pt.upskill.RefugeeLINK.Exceptions.MentorAlreadySelectedException;
 import pt.upskill.RefugeeLINK.Exceptions.MentorIdNotFound;
 import pt.upskill.RefugeeLINK.Models.Mentor;
 import pt.upskill.RefugeeLINK.Models.Refugee;
@@ -89,13 +90,13 @@ public class RefugeeService {
     }
 
     @Transactional
-    public void selectMentorForRefugee(String username, Long mentorId) throws MentorIdNotFound {
+    public void selectMentorForRefugee(String username, Long mentorId) throws MentorIdNotFound, MentorAlreadySelectedException {
 
         // Retrieve the refugee object
         Refugee refugee = getRefugeeByUsername(username);
 
         if (refugee.getMentor() != null) {
-            throw new IllegalArgumentException("Refugee " + username + " already has a mentor.");
+            throw new MentorAlreadySelectedException("Refugee " + username + " already has a mentor.");
         }
         // Retrieve the mentor object
         Mentor mentor = mentorRepository.findById(mentorId)
