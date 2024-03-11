080bb123083f9496334e4b8bc6afb6be0a31669c
Rafa <rafa.figueiredo48@gmail.com>
2024-03-11 10:29:28 +0000

    Organization Login

--- JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Repositories/OrganizationRepository.java
+++ JPA/RefugeeLINK/src/main/java/pt/upskill/RefugeeLINK/Repositories/OrganizationRepository.java
@@ -2,9 +2,14 @@ package pt.upskill.RefugeeLINK.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pt.upskill.RefugeeLINK.Models.Mentor;
 import pt.upskill.RefugeeLINK.Models.Organization;
+
+import java.util.Optional;
 
 @Repository
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+
+    Optional<Organization> findByUserName(String userName);
 
 }
