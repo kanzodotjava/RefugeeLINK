6439440a5a2f820bd5797f844ca465c691db7ec2
franciscomaia097 <105322731+franciscomaia097@users.noreply.github.com>
2024-03-13 15:58:17 +0000

    Rating component

--- refugee-link-angular/src/app/components/rating/rating.component.ts
+++ refugee-link-angular/src/app/components/rating/rating.component.ts
@@ -6,28 +6,38 @@ import { ApiService } from '../../services/ApiService/api.service';
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
-  styleUrls: ['./rating.component.css']
+  styleUrls: ['./rating.component.css'],
 })
 export class RatingComponent {
-
   mentorUsername: any;
   refugeeUsername = this.authService.getUsername();
+  
 
-  constructor(private authService: AuthService, private apiService: ApiService, private http: HttpClient) {
-    this.apiService.getMentorUsernameByUsername(this.refugeeUsername!).subscribe(data => {
-      this.mentorUsername = data.username;
-    }
-    );
+  constructor(
+    private authService: AuthService,
+    private apiService: ApiService,
+    private http: HttpClient
+  ) {
+    this.apiService
+      .getMentorUsernameByUsername(this.refugeeUsername!)
+      .subscribe((data) => {
+        this.mentorUsername = data.username;
+      });
   }
 
   rate(rating: number) {
-    const url = 'http://localhost:7165/rate-mentor';
+    // Convert the rating to a double
+    const doubleRating: number = parseFloat(rating.toFixed(1));
+
+    const url = 'https://localhost:7165/rate-mentor';
     const body = {
-      mentorUsername: this.mentorUsername,
-      refugeeUsername: this.refugeeUsername,
-      rating: rating
+      mentorUsername: 'mentor123',
+      userUsername: 'Refugee1',
+      rating: doubleRating,
     };
 
+    // Log the JSON object before sending the request
+    console.log('JSON to be sent:', body);
 
     this.http.post(url, body).subscribe({
       next: (response) => {
@@ -35,7 +45,7 @@ export class RatingComponent {
       },
       error: (error) => {
         console.error('Failed to submit rating', error);
-      }
+      },
     });
   }
 }
