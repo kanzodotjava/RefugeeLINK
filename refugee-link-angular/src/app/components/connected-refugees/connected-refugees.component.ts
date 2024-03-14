70f2ffb3882da92ee27b31027e2c0f98086810ec
franciscomaia097 <105322731+franciscomaia097@users.noreply.github.com>
2024-03-14 12:18:50 +0000

    Fix connected refugees not refresh

--- refugee-link-angular/src/app/components/connected-refugees/connected-refugees.component.ts
+++ refugee-link-angular/src/app/components/connected-refugees/connected-refugees.component.ts
@@ -1,30 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/ApiService/api.service';
+import { retry, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
 @Component({
   selector: 'app-connected-refugees',
   templateUrl: './connected-refugees.component.html',
   styleUrls: ['./connected-refugees.component.css'],
 })
-export class ConnectedRefugeesComponent implements OnInit{
+export class ConnectedRefugeesComponent implements OnInit {
   refugeeInfoList: any[] = [];
   profilePictureUrl: string = './assets/images/pfp/';
 
-  constructor(private apiService: ApiService) {  }
+  constructor(private apiService: ApiService) {}
 
-  
   ngOnInit(): void {
-    // Get username from local storage
     const username = localStorage.getItem('username');
     if (username) {
-      // Make API call to fetch list of refugees
-      this.apiService.getRefugeesByMentorUsername(username).subscribe(
-        (response: any[]) => {
+      this.apiService
+        .getRefugeesByMentorUsername(username)
+        .pipe(
+          retry(3), // Retry the request up to 3 times
+          catchError((error) => {
+            console.error('Error fetching refugees:', error);
+            // Handle the error here, e.g., display a message to the user
+            return throwError(error);
+          })
+        )
+        .subscribe((response: any[]) => {
           this.refugeeInfoList = response;
-        },
-        (error) => {
-          console.error('Error fetching refugees:', error);
-        }
-      );
+        });
     } else {
       console.error('Username not found in local storage');
     }
